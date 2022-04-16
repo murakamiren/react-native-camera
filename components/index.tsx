ecf9ae66a0fab2b27d8b8b9a448397661bb93cfe
ren <autoeyy@gmail.com>
2022-04-16 21:37:54 +0900

    set camera

--- components/index.tsx
+++ components/index.tsx
@@ -1,10 +1,19 @@
 import { Camera } from "expo-camera";
-import { View } from "native-base";
+import { Button, Text, View } from "native-base";
 import { useEffect, useState, VFC } from "react";
+import { TouchableOpacity } from "react-native";
 
 const Index: VFC = () => {
     const [hasPermission, setHasPermission] = useState<null | boolean>(null);
     const [type, setType] = useState(Camera.Constants.Type.back);
+
+    const handleType = () => {
+        setType(
+            type === Camera.Constants.Type.back
+                ? Camera.Constants.Type.front
+                : Camera.Constants.Type.back
+        );
+    };
 
     useEffect(() => {
         (async () => {
@@ -16,7 +25,17 @@ const Index: VFC = () => {
     if (hasPermission === null) {
         return <View />;
     }
-    return <View></View>;
+    if (hasPermission === false) {
+        return <Text>no access to camera</Text>;
+    }
+    return (
+        <View flex="1">
+            <Camera type={type} style={{ flex: 1 }} />
+            <View>
+                <Button onPress={handleType}>flip</Button>
+            </View>
+        </View>
+    );
 };
 
 export default Index;
